89f191665539f81baaa0a5e1b15d90a7de3d8191
Nicholas Tang Siong Ping <131934941+NicholasTangSiongPing2008@users.noreply.github.com>
2023-10-27 23:07:44 +0800

    Update Contoh_4_19.py 1

--- Contoh_4_19.py
+++ Contoh_4_19.py
@@ -1,1 +1,16 @@
 #Contoh_4_19
+def kos(paparan):
+    print(paparan)
+    h=float(input())
+    return h
+def main():
+    h1=kos("Masukkan harga kos RM:")
+    h2=kos("Masukkan harga jualan RM:")
+    if h1==h2:
+        print("Tiada keuntungan.")
+    elif h2 > h1:
+        print("Peratus keuntungan:",((h2-h1)/h1)*100,"%")
+    else:
+        print("Peratus kerugian:",((h1-h2)/h1)*100,"%")
+if __name__=="__main__":
+  main()
